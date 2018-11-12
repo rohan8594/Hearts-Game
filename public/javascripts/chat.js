39fd380b1d6fb7fdacc440ba27d12bf59da7a899
rohan8594 <rohan.8594@gmail.com>
2018-11-11 17:59:45 -0800

    Updates to chat
    - Fixed lobby entry msg
    - Broadcasting msg when user types

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -3,7 +3,12 @@ const chatSocket = io();
 // Query DOM
 let msg = document.getElementById('message'),
     btn = document.getElementById('send'),
-    output = document.getElementById('output');
+    output = document.getElementById('output'),
+    feedback = document.getElementById('feedback');
+
+window.addEventListener('load', () => {
+    chatSocket.emit('entered lobby', username);
+});
 
 btn.addEventListener('click', () => {
     chatSocket.emit('chat', {
@@ -12,12 +17,22 @@ btn.addEventListener('click', () => {
     })
 });
 
+msg.addEventListener('keypress', () => {
+   chatSocket.emit('typing', username)
+});
+
 // Listen for events
 chatSocket.on('send msg', (data) => {
     const { handle, message } = data;
+
+    feedback.innerText = '';
     output.innerHTML += '<p><strong>' + handle + ': </strong>' + message + '</p>'
 });
 
-chatSocket.on('entry msg', () => {
-    output.innerHTML += '<p>' + username + ' has entered the room...</p>'
+chatSocket.on('entry msg', (data) => {
+    output.innerHTML += '<p style="color: #aaa;"><em>' + data + ' has entered the room...</em></p>'
+});
+
+chatSocket.on('typing msg', (data) => {
+   feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>'
 });
--- sockets/index.js
+++ sockets/index.js
@@ -3,10 +3,17 @@ io = require('socket.io')();
 io.on('connection', (socket) => {
     console.log('Made a socket connection');
 
-    socket.emit('entry msg');
+    socket.on('entered lobby', (data) => {
+        console.log('Entered');
+        io.emit('entry msg', data);
+    });
 
     socket.on('chat', (data) => {
         io.emit('send msg', data);
+    });
+
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing msg', data);
     })
 });
 
