268fd54fe511f649338844c2e26c2be36990bb28
rohan8594 <rohan.8594@gmail.com>
2018-11-19 12:37:20 -0800

    Added validation check for empty game name

--- routes/lobby.js
+++ routes/lobby.js
@@ -19,8 +19,9 @@ lobbySocket.on('connection', (socket) => {
 router.get('/', (req, res) => {
     if (req.isAuthenticated()) {
         const { user } = req;
+        const passedError = req.query.error;
 
-        res.render('lobby', {user: user, title: 'Hearts Game'});
+        res.render('lobby', {user: user, title: 'Hearts Game', error: passedError});
     } else {
         res.redirect('/');
     }
@@ -30,17 +31,25 @@ router.post('/createGame', (req, res) => {
     const { user } = req;
     const { max_players, game_name } = req.body;
 
-    Game.createGame(max_players, user.user_id, game_name)
-        .then((results) => {
-            const { game_id } = results[0];
-            Game.createInitialGamePlayer(user.user_id, game_id)
-                .then(() => {
-                    displayGameList();
-                    res.redirect(`/game/${game_id}`);
-                })
-                .catch((error) => { console.log(error) })
-        })
-        .catch((error) => { console.log(error) })
+    req.checkBody('game_name', 'Game name cannot be empty').notEmpty();
+    const errors = req.validationErrors();
+
+    if (errors) {
+        const errStr = encodeURIComponent(errors[0].msg);
+        res.redirect('/lobby?error=' + errStr);
+    } else {
+        Game.createGame(max_players, user.user_id, game_name)
+            .then((results) => {
+                const { game_id } = results[0];
+                Game.createInitialGamePlayer(user.user_id, game_id)
+                    .then(() => {
+                        displayGameList();
+                        res.redirect(`/game/${game_id}`);
+                    })
+                    .catch((error) => { console.log(error) })
+            })
+            .catch((error) => { console.log(error) })
+    }
 });
 
 module.exports = router;
