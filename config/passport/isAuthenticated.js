bebad2495c060a056a6a8439beb4237607cee4e3
rohan8594 <rohan.8594@gmail.com>
2018-11-21 17:25:31 -0800

    Refactored isAuthenticated into middleware function

---
+++ config/passport/isAuthenticated.js
@@ -0,0 +1,9 @@
+const isAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        next();
+    } else {
+        res.redirect('/');
+    }
+};
+
+module.exports = isAuthenticated;
