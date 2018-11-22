0ff3ff2bb57902aeed9836ed187813eec5d9f41f
rohan8594 <rohan.8594@gmail.com>
2018-11-22 13:57:50 -0800

    Added game_observers and user_game_cards migrations

---
+++ migrations/20181122213716-game_observers.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+      return queryInterface.createTable(
+          'game_observers',
+          {
+              user_id: {
+                  type: Sequelize.INTEGER,
+                  allowNull: false,
+                  references: {
+                      model: 'users',
+                      key: 'user_id'
+                  }
+              },
+              game_id: {
+                  type: Sequelize.INTEGER,
+                  allowNull: false,
+                  references: {
+                      model: 'games',
+                      key: 'game_id'
+                  }
+              }
+          }
+      );
+  },
+
+  down: (queryInterface, Sequelize) => {
+      return queryInterface.dropTable('game_observers');
+  }
+};
